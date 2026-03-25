7c9227e5b8420740c0b9e1e10ae6928dc6895013
LengyelMate <lengyelmate22@gmail.com>
2026-03-25 08:53:53 +0100

    Add project files.

---
+++ Focicsapat_menedzser/Jatekos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Focicsapat_menedzser
+{
+
+    public abstract class CsapatTag
+    {
+        private string nev;
+
+        public string Nev
+        {
+            get => nev;
+            set => nev = value;
+        }
+
+        public abstract string GetInfo();
+    }
+
+    public class Jatekos : CsapatTag
+    {
+        public string Poszt { get; set; }
+        public int Golok { get; set; }
+        public int Passzok { get; set; }
+
+        public Jatekos(string nev, string poszt, int golok, int passzok)
+        {
+            this.Nev = nev;
+            this.Poszt = poszt;
+            this.Golok = golok;
+            this.Passzok = passzok;
+        }
+
+        public override string GetInfo() => $"{Nev} ({Poszt})";
+
+        public string ToCsv() => $"{Nev};{Poszt};{Golok};{Passzok}";
+    }
+}
---
+++ Focicsapat_menedzser/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Focicsapat_menedzser
+{
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Jatekos> keret = new ObservableCollection<Jatekos>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgJatekosok.ItemsSource = keret;
+        }
+
+        private void BtnMentes_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("keret_adatok.csv"))
+                {
+                    foreach (var j in keret) sw.WriteLine(j.ToCsv());
+                }
+                MessageBox.Show("Sikeres mentés a keret_adatok.csv fájlba!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba mentés közben: " + ex.Message);
+            }
+        }
+
+        private void BtnBeolvas_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("keret_adatok.csv"))
+            {
+                MessageBox.Show("Nincs még mentett adatfájl!");
+                return;
+            }
+
+            try
+            {
+                keret.Clear();
+                var sorok = File.ReadAllLines("keret_adatok.csv");
+                foreach (var sor in sorok)
+                {
+                    if (string.IsNullOrWhiteSpace(sor)) continue;
+                    var adatok = sor.Split(';');
+                    if (adatok.Length == 4)
+                    {
+                        keret.Add(new Jatekos(adatok[0], adatok[1], int.Parse(adatok[2]), int.Parse(adatok[3])));
+                    }
+                }
+                MessageBox.Show("Adatok sikeresen betöltve!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba beolvasáskor: " + ex.Message);
+            }
+        }
+
+        private void BtnTorles_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgJatekosok.SelectedItem is Jatekos kijelolt)
+            {
+                keret.Remove(kijelolt);
+            }
+            else
+            {
+                MessageBox.Show("Válassz ki egy játékost a listából!");
+            }
+        }
+    }
+}
